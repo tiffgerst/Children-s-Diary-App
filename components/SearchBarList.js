8b2237bb2a103d16cb98b59a11fb481463889d51
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2022-03-10 15:53:02 +0000

    calendar-button-bug-fix

--- components/SearchBarList.js
+++ components/SearchBarList.js
@@ -1,64 +1,133 @@
-import React from "react";
-import { StyleSheet, Text, Image, View, FlatList, SafeAreaView } from "react-native";
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  FlatList,
+  SafeAreaView,
+} from 'react-native'
 import { theme } from '../src/core/theme'
-import TagButton from '../components/TagButton'
+import TagButton from './TagButton'
 
-const Item = ({ journalDate, journalTitle, journalText, journalImage, journalTag }) => (
-  <View style={styles.container}>
-    <Text style={styles.date}>{journalDate}</Text>
-    <Text style={styles.title}>{journalTitle}</Text>
-    <Image style={styles.image} source={journalImage}/>
-    <Text style={styles.text} numberOfLines={1}>{journalText}</Text>
-    <TagButton style={styles.tag} mode="contained">{journalTag}</TagButton>
-    <Text style={{paddingBottom: 10 }}/>
-  </View>
-);
+function Item({
+  journalDate,
+  journalTitle,
+  journalText,
+  journalImage,
+  journalTag,
+}) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.date}>{journalDate}</Text>
+      <Text style={styles.title}>{journalTitle}</Text>
+      <Image style={styles.image} source={journalImage} />
+      <Text style={styles.text} numberOfLines={1}>
+        {journalText}
+      </Text>
+      <TagButton style={styles.tag} mode="contained">
+        {journalTag}
+      </TagButton>
+      <Text style={{ paddingBottom: 10 }} />
+    </View>
+  )
+}
 
 // the filter
-const SearchBarList = ({ searchPhrase, setClicked, data }) => {
+function SearchBarList({ searchPhrase, setClicked, data }) {
   const renderItem = ({ item }) => {
     // when no input, show all
-    if (searchPhrase === "") {
+    if (searchPhrase === '') {
       return (
         <View>
-          <Item journalDate={item.journalDate} journalTitle={item.journalTitle} journalText={item.journalText} journalImage={item.journalImage} journalTag={item.journalTag} />
+          <Item
+            journalDate={item.journalDate}
+            journalTitle={item.journalTitle}
+            journalText={item.journalText}
+            journalImage={item.journalImage}
+            journalTag={item.journalTag}
+          />
         </View>
-      )}
+      )
+    }
     // filter of the date
-    if (item.journalDate.toUpperCase().includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
+    if (
+      item.journalDate
+        .toUpperCase()
+        .includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ''))
+    ) {
       return (
         <View>
-          <Item journalDate={item.journalDate} journalTitle={item.journalTitle} journalText={item.journalText} journalImage={item.journalImage} journalTag={item.journalTag} />
+          <Item
+            journalDate={item.journalDate}
+            journalTitle={item.journalTitle}
+            journalText={item.journalText}
+            journalImage={item.journalImage}
+            journalTag={item.journalTag}
+          />
         </View>
-      )}    
+      )
+    }
     // filter of the title
-    if (item.journalTitle.toUpperCase().includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
+    if (
+      item.journalTitle
+        .toUpperCase()
+        .includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ''))
+    ) {
       return (
         <View>
-          <Item journalDate={item.journalDate} journalTitle={item.journalTitle} journalText={item.journalText} journalImage={item.journalImage} journalTag={item.journalTag} />
+          <Item
+            journalDate={item.journalDate}
+            journalTitle={item.journalTitle}
+            journalText={item.journalText}
+            journalImage={item.journalImage}
+            journalTag={item.journalTag}
+          />
         </View>
-      )}
+      )
+    }
     // filter of the content
-    if (item.journalText.toUpperCase().includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
+    if (
+      item.journalText
+        .toUpperCase()
+        .includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ''))
+    ) {
       return (
         <View>
-          <Item journalDate={item.journalDate} journalTitle={item.journalTitle} journalText={item.journalText} journalImage={item.journalImage} journalTag={item.journalTag} />
+          <Item
+            journalDate={item.journalDate}
+            journalTitle={item.journalTitle}
+            journalText={item.journalText}
+            journalImage={item.journalImage}
+            journalTag={item.journalTag}
+          />
         </View>
-      )}
+      )
+    }
     // filter of the tag
-    if (item.journalTag.toUpperCase().includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
+    if (
+      item.journalTag
+        .toUpperCase()
+        .includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ''))
+    ) {
       return (
         <View>
-          <Item journalDate={item.journalDate} journalTitle={item.journalTitle} journalText={item.journalText} journalTag={item.journalTag} />
+          <Item
+            journalDate={item.journalDate}
+            journalTitle={item.journalTitle}
+            journalText={item.journalText}
+            journalTag={item.journalTag}
+          />
         </View>
-      )}
-  };
+      )
+    }
+  }
 
   return (
     <SafeAreaView>
       <View
         onStartShouldSetResponder={() => {
-          setClicked(false);
+          setClicked(false)
         }}
       >
         <FlatList
@@ -71,59 +140,59 @@ const SearchBarList = ({ searchPhrase, setClicked, data }) => {
         />
       </View>
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default SearchBarList;
+export default SearchBarList
 
 const styles = StyleSheet.create({
-    container: {
-      width: '90%',
-      alignSelf: 'center',
-      marginHorizontal: 5,
-      marginVertical: 8,
-      borderRadius: 12,
-      backgroundColor: theme.colors.tint,
-      borderWidth: 3,
-      borderColor: '#ddd',
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 1.5 },
-      shadowOpacity: 0.25,
-    },
-    grid: {
-      width: '100%',
-      paddingBottom: 300,
-    },
-    date: {
-      fontSize: 13, 
-      color: '#5A6174',
-      padding: 1,
-      top: 8,
-      right: 10,
-      alignSelf: 'flex-end',
-      }, 
-    title: {
-      fontWeight: 'bold', 
-      color: '#5A6174',
-      padding: 1,
-      left: 10,
-      },
-    text: {
-      fontSize: 13, 
-      color: '#5A6174',
-      width: '95%',
-      padding: 1,
-      top: 5,
-      left: 10,
-      },
-    image: {
-      width: '95%',
-      alignSelf: 'center',
-      top: 2,
-      borderRadius: 12,
-      },
-    tag: {
-      top: 15,
-      left: 10,
-      },   
-  });
+  container: {
+    width: '90%',
+    alignSelf: 'center',
+    marginHorizontal: 5,
+    marginVertical: 8,
+    borderRadius: 12,
+    backgroundColor: theme.colors.tint,
+    borderWidth: 3,
+    borderColor: '#ddd',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1.5 },
+    shadowOpacity: 0.25,
+  },
+  grid: {
+    width: '100%',
+    paddingBottom: 300,
+  },
+  date: {
+    fontSize: 13,
+    color: '#5A6174',
+    padding: 1,
+    top: 8,
+    right: 10,
+    alignSelf: 'flex-end',
+  },
+  title: {
+    fontWeight: 'bold',
+    color: '#5A6174',
+    padding: 1,
+    left: 10,
+  },
+  text: {
+    fontSize: 13,
+    color: '#5A6174',
+    width: '95%',
+    padding: 1,
+    top: 5,
+    left: 10,
+  },
+  image: {
+    width: '95%',
+    alignSelf: 'center',
+    top: 2,
+    borderRadius: 12,
+  },
+  tag: {
+    top: 15,
+    left: 10,
+  },
+})
--- screens/Home.js
+++ screens/Home.js
@@ -1,123 +1,136 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Image, View, Text, FlatList, TouchableOpacity } from 'react-native' 
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
 import { theme } from '../src/core/theme'
 import Background3 from '../components/Background3'
 import CalendarButton from '../components/CalendarButton'
 import ProfileButton from '../components/ProfileButton'
-import { getStatusBarHeight } from 'react-native-status-bar-height'
 import SearchBar from '../components/SearchBar'
 import SearchBarList from '../components/SearchBarList'
 import JournalFeed from '../components/JournalFeed'
 
-
 const postData = [
-  { postId: 1,
+  {
+    postId: 1,
     journalDate: '30 Aug 2021',
-    journalTitle: '3 Good things for today', 
-    journalText: 'It could be anything from hanging out with friends...' ,
-    journalTag: 'Sports'},
-  { postId: 2,
+    journalTitle: '3 Good things for today',
+    journalText: 'It could be anything from hanging out with friends...',
+    journalTag: 'Sports',
+  },
+  {
+    postId: 2,
     journalDate: '31 Mar 2021',
-    journalTitle: 'My feelings', 
-    journalText: 'I feel excited and calm.' ,
-    journalTag: ''},
-  { postId: 3,
+    journalTitle: 'My feelings',
+    journalText: 'I feel excited and calm.',
+    journalTag: '',
+  },
+  {
+    postId: 3,
     journalDate: '26 Mar 2021',
-    journalTitle: 'Vacation', 
-    journalText: 'Summer is the warmest of the four seasons. Spring' ,
-    journalTag: 'Friends'},
-  { postId: 4,
+    journalTitle: 'Vacation',
+    journalText: 'Summer is the warmest of the four seasons. Spring',
+    journalTag: 'Friends',
+  },
+  {
+    postId: 4,
     journalDate: '20 Mar 2021',
-    journalTitle: 'My lovely dogge', 
-    journalText: 'Had this dog around growing up' ,
-    journalImage: require('../assets/journal_image_example.png'),     
-    journalTag: 'Family'},
-  ]
-
-  
-
+    journalTitle: 'My lovely dogge',
+    journalText: 'Had this dog around growing up',
+    journalImage: require('../assets/journal_image_example.png'),
+    journalTag: 'Family',
+  },
+]
 
 export default function Home({ navigation }) {
-  const [searchPhrase, setSearchPhrase] = useState("");
-  const [clicked, setClicked] = useState(false);
-  const [fakeData, setFakeData] = useState();
+  const [searchPhrase, setSearchPhrase] = useState('')
+  const [clicked, setClicked] = useState(false)
+  const [fakeData, setFakeData] = useState()
 
-  return (    
+  return (
     <Background3 style={styles.background}>
-      <TouchableOpacity onPress={() => navigation.navigate('Calendar')} style={styles.calendar}>
-        <Image source={require('../assets/calendar_button.png')}/>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Calendar')}
+        style={styles.calendar}
+      >
+        <Image source={require('../assets/calendar_button.png')} />
       </TouchableOpacity>
       <View>
         <Text style={styles.title}>Journal</Text>
       </View>
-      <TouchableOpacity onPress={() => navigation.navigate('ProfileFeed')} style={styles.profile}>
-        <Image source={require('../assets/profile_button.png')}/>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('ProfileFeed')}
+        style={styles.profile}
+      >
+        <Image source={require('../assets/profile_button.png')} />
       </TouchableOpacity>
-      <View>
-        {!clicked }
-        <SearchBar
+      {!clicked}
+      <SearchBar
+        searchPhrase={searchPhrase}
+        setSearchPhrase={setSearchPhrase}
+        clicked={clicked}
+        setClicked={setClicked}
+      />
+      <View style={styles.scroll}>
+        <SearchBarList
           searchPhrase={searchPhrase}
-          setSearchPhrase={setSearchPhrase}
-          clicked={clicked}
+          data={postData}
           setClicked={setClicked}
         />
-        {(
-          <View style={styles.scroll}>
-            <SearchBarList
-              searchPhrase={searchPhrase}
-              data={postData}
-              setClicked={setClicked}
-            />
-          </View>
-        )}
       </View>
     </Background3>
   )
 }
 
-
 const styles = StyleSheet.create({
-    background: {
-      flex: 1,
-      width: '90%',
-      height: '90%',
-      // backgroundColor: theme.colors.tint,
-      alignSelf: 'center',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-    },
-    title: {
-      fontSize: 22,
-      fontWeight: 'bold',
-      color: theme.colors.secondary,
-      top: 40 + getStatusBarHeight(),
-      }, 
-    calendar: {
-      position: 'absolute',
-      top: 10 + getStatusBarHeight(),
-      left: 4,
-    },  
-    profile: {
-      position: 'absolute',
-      top: 10 + getStatusBarHeight(),
-      right: 4,
-    },  
-    // search: {
-    //   flex: 1,
-    //   position: 'relative',
-    //   // top: 30 + getStatusBarHeight(),
-    //   lineHeight: 1 ,
-    //   right: 4,
-    // }, 
-    // SearchContainer: {
-    //   flex: 1,
-    //   position: 'relative',
-    //   top: 60 + getStatusBarHeight(),
-    //   right: 4,
-    //   width:'100%', 
-    //   height: '10%',
-    // },  
-    scroll: {
-      top: 62 + getStatusBarHeight(),  
-    },
-  })
+  background: {
+    flex: 1,
+    width: '90%',
+    height: '90%',
+    // backgroundColor: theme.colors.tint,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: theme.colors.secondary,
+    top: 40 + getStatusBarHeight(),
+  },
+  calendar: {
+    position: 'absolute',
+    top: 10 + getStatusBarHeight(),
+    left: 4,
+  },
+  profile: {
+    position: 'absolute',
+    top: 10 + getStatusBarHeight(),
+    right: 4,
+  },
+  // search: {
+  //   flex: 1,
+  //   position: 'relative',
+  //   // top: 30 + getStatusBarHeight(),
+  //   lineHeight: 1 ,
+  //   right: 4,
+  // },
+  // SearchContainer: {
+  //   flex: 1,
+  //   position: 'relative',
+  //   top: 60 + getStatusBarHeight(),
+  //   right: 4,
+  //   width:'100%',
+  //   height: '10%',
+  // },
+  scroll: {
+    top: 62 + getStatusBarHeight(),
+    minWidth: '125%',
+  },
+})
