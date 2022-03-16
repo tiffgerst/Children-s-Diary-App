22682ca7deb1e3b86449fe05f4ef85ebfba98251
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2022-03-16 16:30:03 +0000

    port-update

--- API/index.js
+++ API/index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("This is the home page!");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>
   console.log(`Listening on port http://localhost:${PORT}`)
