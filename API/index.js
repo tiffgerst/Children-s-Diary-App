23e62ab193b36b9429729936b9b031c97d889c0d
Dora <dorayin0602@gmail.com>
2022-03-23 18:34:26 +0000

    reformatted code

--- API/index.js
+++ API/index.js
@@ -2,8 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import usersRoutes from "./routes/appUser.js";
 import postsRoutes from "./routes/post.js";
-import tagsRoutes from "./routes/post_tag.js";
-import imagesRoutes from "./routes/postImageUploaded.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,8 +9,6 @@ app.use(bodyParser.json());
 //every route inside the post route is accessed with /researcher
 app.use("/appUser", usersRoutes);
 app.use("/post", postsRoutes);
-app.use("/post_tag", tagsRoutes);
-app.use("/postImageUploaded", imagesRoutes);
 
 app.get("/", (req, res) => {
   res.send("This is the home page!");
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   StyleSheet,
   Image,
   View,
   Text,
-  FlatList,
   TouchableOpacity,
 } from 'react-native'
 import { getStatusBarHeight } from 'react-native-status-bar-height'
@@ -12,41 +11,7 @@ import { theme } from '../src/core/theme'
 import Background3 from '../components/Background3'
 import SearchBar from '../components/SearchBar'
 import SearchBarList from '../components/SearchBarList'
-import JournalFeed from '../components/JournalFeed'
 import AddButton from '../components/AddButton'
-
-// const postData = [
-//   {
-//     postID: 1,
-//     createDateTime: '30 Aug 2021',
-//     titleText: '3 Good things for today',
-//     contentText: 'It could be anything from hanging out with friends...',
-//     // imageURL: '',
-//     // tagNameAll: 'Sports',
-//   },
-//   {
-//     postID: 2,
-//     createDateTime: '31 Mar 2021',
-//     titleText: 'My feelings',
-//     contentText: 'I feel excited and calm.',
-//     // journalTag: 'Sports',
-//   },
-//   {
-//     postID: 3,
-//     createDateTime: '26 Mar 2021',
-//     titleText: 'Vacation',
-//     contentText: 'Summer is the warmest of the four seasons. Spring',
-//     // journalTag: 'Friends',
-//   },
-//   {
-//     postID: 4,
-//     createDateTime: '20 Mar 2021',
-//     titleText: 'My lovely dogge',
-//     contentText: 'Had this dog around growing up',
-//     imageURL: 'https://csb100320016de6b123.blob.core.windows.net/post-image/doggie.png?sp=r&st=2022-03-20T03:11:07Z&se=2022-04-30T10:11:07Z&spr=https&sv=2020-08-04&sr=b&sig=2ngZyIk6E2ttC51zpIs8qDtYr3bj5J9uskoKOi1ZfE8%3D',
-//     // journalTag: 'Family',
-//   },
-// ]
 
 export default function Home({ navigation }) {
   const [searchPhrase, setSearchPhrase] = useState('')
